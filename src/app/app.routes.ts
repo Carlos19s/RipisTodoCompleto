ca046456265242a258b3f8b72fce32043c831e9c
Carlos <carlosg72quintana@gmail.com>
2025-04-18 08:40:59 -0500

    Mejorado navegacion y Boton Registro, Implementado BeneficiosRed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactoComponent } from './contacto/contacto.component';
 import {InicioComponent} from './inicio/inicio.component';
 import {SobreNosotrosComponent} from './sobre-nosotros/sobre-nosotros.component';
+import {BeneficiosComponent} from './beneficios/beneficios.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
   { path: 'contacto', component: ContactoComponent },
   { path: 'sobre-nosotros', component: SobreNosotrosComponent },
+  {path: 'beneficios', component: BeneficiosComponent },
 ];
 
 @NgModule({
