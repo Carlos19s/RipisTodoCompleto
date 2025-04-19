172670d2befe7f59aad2e53dbf87796bea96a97b
Carlos <carlosg72quintana@gmail.com>
2025-04-18 20:56:39 -0500

    TodoLosProyectosUnidos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import {InicioComponent} from './inicio/inicio.component';
 import {SobreNosotrosComponent} from './sobre-nosotros/sobre-nosotros.component';
 import {BeneficiosComponent} from './beneficios/beneficios.component';
 import {UnialiadasComponent} from './unialiadas/unialiadas.component';
+import {MiembrosComponent} from './miembros/miembros.component';
+import {EstatutosComponent} from './estatutos/estatutos.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
@@ -12,6 +14,8 @@ export const routes: Routes = [
   { path: 'sobre-nosotros', component: SobreNosotrosComponent },
   {path: 'beneficios', component: BeneficiosComponent },
   {path: 'Unialiadas', component: UnialiadasComponent },
+  {path: 'Miembros', component: MiembrosComponent },
+  {path: 'Estatutos', component: EstatutosComponent },
 ];
 
 @NgModule({
