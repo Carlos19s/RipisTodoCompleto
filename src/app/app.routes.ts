d02af58a67a194e5a95aa1e560471fc1e81f0c8a
Adrian Urresta <152809078+Mclxv1nx@users.noreply.github.com>
2025-04-22 23:56:30 -0500

    Página de Perfil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,32 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactoComponent } from './contacto/contacto.component';
-import {InicioComponent} from './inicio/inicio.component';
-import {SobreNosotrosComponent} from './sobre-nosotros/sobre-nosotros.component';
-import {BeneficiosComponent} from './beneficios/beneficios.component';
-import {UnialiadasComponent} from './unialiadas/unialiadas.component';
-import {MiembrosComponent} from './miembros/miembros.component';
-import {EstatutosComponent} from './estatutos/estatutos.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
+import { BeneficiosComponent } from './beneficios/beneficios.component';
+import { UnialiadasComponent } from './unialiadas/unialiadas.component';
+import { MiembrosComponent } from './miembros/miembros.component';
+import { EstatutosComponent } from './estatutos/estatutos.component';
 import { RegistroComponent } from './registro/registro.component';
-import {PlanificacionActividadesComponent} from './planificacion-actividades/planificacion-actividades.component';
+import { PlanificacionActividadesComponent } from './planificacion-actividades/planificacion-actividades.component';
 import { BlogComponent } from './blog/blog.component';
 import { OdsComponent } from './ods/ods.component';
 import { ErrorComponent } from './error/error.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
   { path: 'contacto', component: ContactoComponent },
   { path: 'sobre-nosotros', component: SobreNosotrosComponent },
-  {path: 'beneficios', component: BeneficiosComponent },
-  {path: 'Unialiadas', component: UnialiadasComponent },
-  {path: 'Miembros', component: MiembrosComponent },
-  {path: 'Estatutos', component: EstatutosComponent },
-  {path: 'Registro', component:  RegistroComponent},
-  {path: 'Planificacion', component:  PlanificacionActividadesComponent},
-  {path: 'Blog', component: BlogComponent},
-  {path: 'Ods', component: OdsComponent},
-  { path: '**', component: ErrorComponent }
+  { path: 'beneficios', component: BeneficiosComponent },
+  { path: 'Unialiadas', component: UnialiadasComponent },
+  { path: 'Miembros', component: MiembrosComponent },
+  { path: 'Estatutos', component: EstatutosComponent },
+  { path: 'Registro', component: RegistroComponent },
+  { path: 'Planificacion', component: PlanificacionActividadesComponent },
+  { path: 'Blog', component: BlogComponent },
+  { path: 'Ods', component: OdsComponent },
+  { path: 'perfil', component: PerfilComponent },
 
+
+
+
+  { path: '**', component: ErrorComponent }
 ]
 
 @NgModule({
